2eaf0053b5d92f2d01104b8b9d53dc44e24b9b25
cailin <852686396@qq.com>
2016-06-15 15:55:08 +0800

    fix bugs

--- app/email.py
+++ app/email.py
@@ -5,11 +5,12 @@ from threading import Thread
 
 
 def send_email(to, subject, template, **kwargs):
-    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
-                  sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[current_app, msg])
+    thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
 
